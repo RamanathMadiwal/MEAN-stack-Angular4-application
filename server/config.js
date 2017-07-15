932044cbdca1e9c21e44717128eaef50311840d0
Ramanath Madiwal <ramanathrm.kumta@gmail.com>
2017-07-15 15:14:41 +0530

    commit

--- server/config.js
+++ server/config.js
@@ -2,5 +2,5 @@ module.exports = {
    'serverport':1978,
    'tokenexp': 3600,
    'secret': 'mysecretkey',
-   'database': 'mongodb://SuperAdmin:Pleasewait@ds013014.mlab.com:13014/expensedb'
+   'database': 'mongodb://localhost:27014/expensedb'//add mlab url
 };
